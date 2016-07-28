42476b80040b7328c19d0820edccb51dbaf277a4
刘小飞 <275570823@qq.com>
2016-07-28 15:48:52 +0800

    创建文件夹分组

---
+++ SJQBasicKit/Classes/Controllers/SJQBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  SJQBaseViewController.h
+//  Pods
+//
+//  Created by Felix on 16/7/28.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SJQBaseViewController : UIViewController
+
+@end
---
+++ SJQBasicKit/Classes/Controllers/SJQBaseViewController.m
@@ -0,0 +1,13 @@
+//
+//  SJQBaseViewController.m
+//  Pods
+//
+//  Created by Felix on 16/7/28.
+//
+//
+
+#import "SJQBaseViewController.h"
+
+@implementation SJQBaseViewController
+
+@end
